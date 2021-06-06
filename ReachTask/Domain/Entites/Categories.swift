c242024d2f29198f598b488d476cd9a2399ba810
Sherif Kamal <s.kamal@aqwas.sa>
2021-06-06 18:59:22 +0200

    Create Categories entity

---
+++ ReachTask/Domain/Entites/Categories.swift
@@ -0,0 +1,32 @@
+//
+//  Entities.swift
+//  ReachTask
+//
+//  Created by sherif kamal on 06/06/2021.
+//
+
+import Foundation
+// MARK: - JBETACostRequest
+struct Categories {
+    let data: [Category]
+    let meta: Meta
+}
+
+// MARK: - JBETACostRequestDatum
+struct Category {
+    let id: Int
+    let slug, name: String
+    let users: Users
+}
+
+// MARK: - Meta
+struct Meta {
+    let pagination: Pagination
+}
+
+// MARK: - Pagination
+struct Pagination {
+    let total, count, perPage, currentPage: Int
+    let totalPages: Int
+    let links: String?
+}
