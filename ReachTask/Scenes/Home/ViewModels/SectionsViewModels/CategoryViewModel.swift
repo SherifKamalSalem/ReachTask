acd444932513f9255e0e1484dcaf5afa668aafaa
Sherif Kamal <s.kamal@aqwas.sa>
2021-06-06 23:23:59 +0200

    Create 'CategoryViewModel'

---
+++ ReachTask/Scenes/Home/ViewModels/SectionsViewModels/CategoryViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CategoryViewModel.swift
+//  ReachTask
+//
+//  Created by sherif kamal on 06/06/2021.
+//
+
+import Foundation
+
+final class CategoryViewModel {
+    let id: Int
+    let slug: String
+    let usersViewModels: [UserViewModel]
+    var profilePicture: URL? = nil
+    var coverPhoto: URL? = nil
+    
+    init(with category: Category) {
+        self.id = category.id
+        self.slug = category.slug
+        self.usersViewModels = category.users.data.compactMap { UserViewModel(with: $0) }
+    }
+}
