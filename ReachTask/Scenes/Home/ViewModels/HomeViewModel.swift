f3c910e1ff933c04dc4fe80c8425d6e9430663de
Sherif Kamal <s.kamal@aqwas.sa>
2021-06-06 23:27:13 +0200

    Add transform function to process input and  covert it to output

--- ReachTask/Scenes/Home/ViewModels/HomeViewModel.swift
+++ ReachTask/Scenes/Home/ViewModels/HomeViewModel.swift
@@ -27,4 +27,23 @@ class HomeViewModel: ViewModelType {
     init(useCase: HomeUseCase) {
         self.useCase = useCase
     }
+    
+    func transform(input: Input) -> Output {
+        let activityIndicator = ActivityIndicator()
+        let errorTracker = ErrorTracker()
+        
+        let categories = input.trigger.flatMapLatest {
+            return self.useCase.getCategories()
+                .trackActivity(activityIndicator)
+                .trackError(errorTracker)
+                .asDriverOnErrorJustComplete()
+                .map { $0.data.map { CategoryViewModel(with: $0) } }
+        }
+        let fetching = activityIndicator.asDriver()
+        let errors = errorTracker.asDriver()
+        
+        return Output(fetching: fetching,
+                      categories: categories,
+                      error: errors)
+    }
 }
