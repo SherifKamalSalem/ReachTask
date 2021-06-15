c2f0dfb54b35c1b1dad66bcc946143ff9fe826fa
Sherif Kamal <s.kamal@aqwas.sa>
2021-06-15 23:51:02 +0200

    Setup populateAllCategoriesTableView helper function for populating tableview sections

--- ReachTask/Scenes/Home/ViewController/HomeViewController.swift
+++ ReachTask/Scenes/Home/ViewController/HomeViewController.swift
@@ -28,8 +28,7 @@ class HomeViewController: UIViewController {
     
     private func setupView() {
         tableView.register(nibWithCellClass: CategoryCell.self)
-        tableView.dataSource = nil
-        tableView.delegate = nil
+        tableView.register(nibWithCellClass: OffersCell.self)
         tableView.rx
             .setDelegate(self)
             .disposed(by: disposeBag)
@@ -45,11 +44,19 @@ class HomeViewController: UIViewController {
         let input = HomeViewModel.Input(trigger: Driver.merge(viewWillAppear))
         let output = viewModel.transform(input: input)
         output.categories
-            .debug()
             .drive(onNext: { [weak self] categories in
                 self?.populateAllCategoriesTableView(with: categories)
             })
             .disposed(by: disposeBag)
+        
+        output.offers
+            .debug()
+            .drive(onNext: { [weak self] offers in
+                guard let offers = offers else { return }
+                self?.populateAllCategoriesTableView(or: offers)
+            })
+            .disposed(by: disposeBag)
+        
         
         output.fetching
             .drive(onNext: { [weak self] isFetching in
@@ -66,19 +73,31 @@ class HomeViewController: UIViewController {
 }
 
 extension HomeViewController {
-    private func populateAllCategoriesTableView(with categories: [CategoryViewModel]) {
-        let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, CategoryViewModel>>(configureCell: { _, table, indexPath, item in
-            let cell = table.dequeueReusableCell(withClass: CategoryCell.self)
-            cell.configure(viewModel: item)
-            return cell
+    private func populateAllCategoriesTableView(with categories: [CategoryViewModel] = [], or offers: OffersViewModel? = nil) {
+        tableView.dataSource = nil
+        tableView.delegate = nil
+        var sections = [MultipleSectionModel]()
+        if let offers = offers {
+            sections.append(.offersSection(title: "", items: offers))
+        }
+        sections.append(.categoriesSection(title: "", items: categories))
+        let dataSource = RxTableViewSectionedReloadDataSource<MultipleSectionModel>(configureCell: { datasource, table, indexPath, item in
+            switch datasource[indexPath] {
+            case let .categories(item):
+                let cell = table.dequeueReusableCell(withClass: CategoryCell.self)
+                cell.configure(with: item, indexPath: indexPath)
+                return cell
+            case .offers(items: let items):
+                let cell = table.dequeueReusableCell(withClass: OffersCell.self)
+                cell.configure(with: items)
+                return cell
+            }
         })
         
         dataSource.titleForHeaderInSection = { dataSource, index in
-            return dataSource.sectionModels[index].model.capitalized
+            return dataSource.sectionModels[index].title.capitalized
         }
         
-        
-        let sections = categories.compactMap({ SectionModel(model: $0.slug, items: [$0]) })
         Observable.just(sections)
             .debug()
             .bind(to: tableView.rx.items(dataSource: dataSource))
@@ -95,3 +114,4 @@ extension HomeViewController: UITableViewDelegate {
         return 70
     }
 }
+
